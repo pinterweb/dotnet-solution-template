ab49247ef4692f5672345d5e1afd99efc3425e11
Jeremy Zagorski <jmzagorski@gmail.com>
2020-06-08 10:17:07 -0400

    fix(Logging): Check for null LogEntry object

--- CSharp/src/BusinessApp.App/StringLogEntryFormatter.cs
+++ CSharp/src/BusinessApp.App/StringLogEntryFormatter.cs
@@ -1,9 +1,15 @@
 namespace BusinessApp.App
 {
+    using System;
     using BusinessApp.Domain;
 
     public class StringLogEntryFormatter : ILogEntryFormatter
     {
-        public string Format(LogEntry entry) => entry.ToString();
+        public string Format(LogEntry entry)
+        {
+            if (entry == null) throw new ArgumentNullException(nameof(entry));
+
+            return entry.ToString();
+        }
     }
 }
