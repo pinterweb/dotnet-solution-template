41cf8c1a2420e1a7974f42aaa6c7d22075c7fe83
Jeremy Zagorski <jmzagorski@gmail.com>
2020-06-08 10:30:16 -0400

    fix: Remove new generic constraint, create default

--- CSharp/src/BusinessApp.WebApi/HttpContextExtensions.cs
+++ CSharp/src/BusinessApp.WebApi/HttpContextExtensions.cs
@@ -14,9 +14,7 @@
         /// Deserializes the uri or body depending on the request method
         /// </summary>
         public static T DeserializeInto<T>(this HttpContext context,
-            ISerializer serializer
-        )
-            where T : class, new()
+            ISerializer serializer)
         {
             if (context.Request.Method.Equals("get", StringComparison.OrdinalIgnoreCase) ||
                 context.Request.Method.Equals("delete", StringComparison.OrdinalIgnoreCase)
@@ -25,19 +23,19 @@
                 // TODO might not be able to do Sync. Deserialization see ShelfLife 3.1 commit
                 using (var stream = DeserializeUri(context, serializer))
                 {
-                    return serializer.Deserialize<T>(stream) ?? new T();
+                    return serializer.Deserialize<T>(stream);
                 }
             }
             else if (context.Request.ContentLength == null || context.Request.ContentLength > 0)
             {
-                return serializer.Deserialize<T>(context.Request.Body) ?? new T();
+                return serializer.Deserialize<T>(context.Request.Body);
             }
 
-            return new T();
+            return default(T);
         }
 
         /// <summary>
-        /// Helper class to create the <see cref="ResponseProblemBody.Type />
+        /// Helper class to create the <see cref="ResponseProblemBody.Type"/>
         /// </summary>
         /// <param name="context"></param>
         /// <param name="errorType"></param>
