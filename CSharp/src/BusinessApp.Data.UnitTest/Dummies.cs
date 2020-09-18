2de9b158a0fc5f359c8144c8b0f4f017e2bb1fa5
Jeremy Zagorski <jmzagorski@gmail.com>
2020-09-18 11:51:18 -0400

    test: Remove Data unit tests.
    Tests were all moved, will receive a warning if it is an empty project.
    
    fixes AB#5357

--- CSharp/src/BusinessApp.Data.UnitTest/Dummies.cs
+++
@@ -1,9 +0,0 @@
-namespace BusinessApp.Data.UnitTest
-{
-    using BusinessApp.App;
-
-    public class DummyResponse {}
-
-    public class DummyRequest : Query, IQuery<EnvelopeContract<DummyResponse>> { }
-}
-
