b06ceda4522b62669eed52aeaf4c5d610c8e6169
Jeremy Zagorski <jmzagorski@gmail.com>
2020-12-06 21:18:52 -0500

    refactor(bootstrap): Reorganization layered bootstraps
    Move all bootstrap methods to partial `Bootstrap` class. Begin to
    reorganize bootstrap methods so all compiler directives can be
    consolidated and testing easier
    
    related #4819

--- CSharp/src/BusinessApp.WebApi/Bootstrap.Domain.cs
+++ CSharp/src/BusinessApp.WebApi/Bootstrap.Domain.cs
@@ -8,18 +8,18 @@
     /// <summary>
     /// Allows registering all types that are defined in the domain layer
     /// </summary>
-    public static class DomainLayerBoostrapper
+    public static partial class Bootstrap
     {
-        private static readonly Assembly Assembly = typeof(IEventHandler<>).Assembly;
+        private static readonly Assembly DomainAssembly = typeof(IEventHandler<>).Assembly;
 
-        public static void Bootstrap(Container container, BootstrapOptions options)
+        public static void Domain(Container container, BootstrapOptions options)
         {
             container.NotNull().Expect(nameof(container));
 
             container.Collection.Register(typeof(IEventHandler<>), new[]
             {
-                Assembly,
-                WebApiBootstrapper.Assembly
+                DomainAssembly,
+                StartupAssembly
             }.Concat(options.AppAssemblies).Concat(options.DataAssemblies));
 
             container.Collection.Append(typeof(IEventHandler<>), typeof(DomainEventHandler<>));
