99c33ad9d5a6ca89841df3e14befd78157cd0ccb
Jeremy Zagorski <jmzagorski@protonmail.com>
2021-07-14 13:29:07 -0400

    refactor(macros): Remove macro feature from basic version

--- CSharp/src/BusinessApp.Infrastructure/IMacro.cs
+++
@@ -1,8 +0,0 @@
-namespace BusinessApp.Infrastructure
-{
-    /// <summary>
-    /// Command that expands to running many <typeparam name="TCommand">TCommand</typeparam>
-    /// </summary>
-    public interface IMacro<TCommand>
-    { }
-}
